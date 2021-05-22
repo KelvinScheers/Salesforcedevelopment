42004bd49f41bdb36f317ebbf5edae249cad82e8
Kelvin Scheers <kelvinscheers@gmail.com>
2021-05-22 12:53:16 +0200

    Javascript start

---
+++ Javascript/main.js
@@ -0,0 +1,56 @@
+let getal1 = "tekst";
+
+console.log ("getal1" + getal1);
+
+const getal2 = "test";
+
+console.log("getal2" + getal2);
+
+const getal22 = 50;
+
+console.log("getal22" + getal22); //dit gaat niet omdat er al een getal2 is. vandaar dat ik het getal22 heb genoemd.
+
+let getal3 = 5;
+console.log("getal3" + getal3);
+
+let datatype = true;
+
+console.log("dataype" + typeof datatype);
+
+// let datatype = "";
+
+// console.log("dataype" + typeof datatype);
+
+//let datatype = 1;
+
+//console.log("dataype" + typeof datatype);
+
+//let datatype = 5n;
+
+//console.log("dataype" + typeof datatype);
+
+//let datatype = Symbol("Sym");
+
+//console.log("dataype" + typeof datatype);
+
+let x = 6;
+let y = 8;
+console.log(x + y);
+
+//vervolg van de opdracht console.log(x - y);
+
+//console.log (x * y);
+
+//console.log (x / y);
+
+let a = 3;
+let b = 5;
+let c = 10;
+let d = 2; 
+
+console.log(a + b * c / d);
+
+let number=3;
+number++;
+
+console.log(number);
