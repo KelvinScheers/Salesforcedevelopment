1819e473d91810a56b5b3afde04ea238f55b1b19
Kelvin Scheers <kelvinscheers@gmail.com>
2021-05-28 21:30:42 +0200

    Bezig met arrays tm opdr 5

---
+++ Javascript/array.js
@@ -0,0 +1,16 @@
+let Randomnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(Randomnumbers [4]);
+
+
+let Fruit = ["Appel", "Peer", "Mango", "Aardbei", "Perzik"];
+
+console.log(Fruit [3]); // opdracht 3 = waarde 0 opdracht 4 staat nu goed.
+
+
+let totaal = (Math.round(Math.random() * 7)) % 5;
+console.log(totaal + " = resultaat = " + Fruit[totaal]);
+
+  
+
+
